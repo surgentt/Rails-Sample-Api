621cfca87c433180b699f0a3578d293aa70e7ab3
Thomas Surgent <surgentt@gmail.com>
2016-06-01 15:44:54 -0400

    Ensure the status code is responded to correctly

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::API
   rescue_from ActiveRecord::RecordNotFound, :with => :record_not_found
 
   def record_not_found
-    render json: {data: {}, errors: {status: 404, title: 'record not found'}}
+    render json: {data: {}, errors: {title: 'record not found'}}, status: 404
   end
 
 end
